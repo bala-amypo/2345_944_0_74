2e451efa1d4aa6319940307ca071a384c23dbc10
bala-amypo <bala@amypo.in>
2025-12-17 08:08:35 +0000

    User commit

--- src/main/java/com/example/demo/newentity/NewFileEntity.java
+++ src/main/java/com/example/demo/newentity/NewFileEntity.java
@@ -1,9 +1,15 @@
-public class NewEntity{
+package com.example.demo.newentity
+
+
+public class NewFileEntity{
     @id
+    @GeneratedValue(stratgey=GeneratorType);
     private long id;
     private String name;
      @notblank(message="No blank allowed");
      @email(message="invalid format");
      private String email;
      
+     
+
 }
